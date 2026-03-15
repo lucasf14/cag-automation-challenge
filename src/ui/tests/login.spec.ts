026709e672bdec77d558a0d89a14df5c03f4399f
Lucas Ferreira <lucassky14@gmail.com>
2026-03-15 20:54:36 +0000

    Refactor login page tests to use LoginPage and DashboardPage and add login/logout test

--- src/ui/tests/login.spec.ts
+++ src/ui/tests/login.spec.ts
@@ -1,49 +1,52 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
+import { DashboardPage } from '../pages/dashboardPage';
 
-test('Successful login', async ({ page }) => {
-  const title = page.locator('.oxd-topbar-header-title');
-  const dashboard = page.locator('.orangehrm-dashboard-grid');
-  const sidePanel = page.locator('.oxd-sidepanel');
-  const widgets = page.locator('.orangehrm-dashboard-widget');
-  
+test('Successful login', async ({ page }) => {  
   const loginPage = new LoginPage(page);
+  const dashboardPage = new DashboardPage(page);
   await loginPage.goto(process.env.UI_BASE_URL || '');
   await loginPage.login(process.env.UI_USERNAME || '', process.env.UI_PASSWORD || '');
 
   await expect(page).toHaveURL(/dashboard/);
-  await expect(title).toBeVisible();
-  await expect(title).toHaveText('Dashboard');
-  await expect(sidePanel).toBeVisible();
-  await expect(dashboard).toBeVisible();
-  await expect(widgets.first()).toBeVisible();
-  expect(await widgets.count()).toBeGreaterThan(1);
+  await expect(dashboardPage.title).toBeVisible();
+  await expect(dashboardPage.title).toHaveText('Dashboard');
+  await expect(dashboardPage.sidePanel).toBeVisible();
+  await expect(dashboardPage.dashboard).toBeVisible();
+  await expect(dashboardPage.widgets.first()).toBeVisible();
+  expect(await dashboardPage.widgets.count()).toBeGreaterThan(1);
 });
 
 
-test('Unsuccessful login with invalid credentials', async ({ page }) => {
-  const errorMessage = page.locator('.oxd-alert-content-text');
-  
+test('Unsuccessful login with invalid credentials', async ({ page }) => {  
   const loginPage = new LoginPage(page);
   await loginPage.goto(process.env.UI_BASE_URL || '');
   await loginPage.login('qwerty', '123456');
 
-  await expect(errorMessage).toBeVisible();
-  await expect(errorMessage).toHaveText('Invalid credentials');
+  await expect(loginPage.errorMessage).toBeVisible();
+  await expect(loginPage.errorMessage).toHaveText('Invalid credentials');
 });
 
 
 test('Unsuccessful login with empty fields', async ({ page }) => {  
-  const usernameContainer = page.locator('.oxd-input-group', { hasText: 'Username' });
-  const passwordContainer = page.locator('.oxd-input-group', { hasText: 'Password' });
-  const usernameRequired = usernameContainer.locator('.oxd-input-field-error-message'); 
-  const passwordRequired = passwordContainer.locator('.oxd-input-field-error-message');
-  
   const loginPage = new LoginPage(page);
   await loginPage.goto(process.env.UI_BASE_URL || '');
   await loginPage.login('', '');
 
-  await expect(usernameRequired).toHaveText('Required');
-  await expect(passwordRequired).toHaveText('Required');
+  await expect(loginPage.usernameRequired).toHaveText('Required');
+  await expect(loginPage.passwordRequired).toHaveText('Required');
 });
 
+
+test('Successful login, logout and redirect to login page', async ({ page }) => {  
+  const loginPage = new LoginPage(page);
+  await loginPage.goto(process.env.UI_BASE_URL || '');
+  await loginPage.login(process.env.UI_USERNAME || '', process.env.UI_PASSWORD || '');
+  await expect(page).toHaveURL(/dashboard/);
+
+  await loginPage.logout();
+  await expect(page).toHaveURL(/auth\/login/);
+
+  await expect(loginPage.username).toHaveValue('');
+  await expect(loginPage.password).toHaveValue('');
+});
